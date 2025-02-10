84a1eec3b423a3e1d8e6ade52dd3ad435b727fe5
Anderson <loonander@gmail.com>
2025-02-10 15:14:39 -0300

    fix: remove console

--- src/app/modules/auth/error.page.tsx
+++ src/app/modules/auth/error.page.tsx
@@ -3,8 +3,6 @@ import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
 function ErrorPage() {
  const error = useRouteError();
 
- console.log(error);
-
  return (
   <div>
    <h1>Ocorreu um erro!!!!</h1>
