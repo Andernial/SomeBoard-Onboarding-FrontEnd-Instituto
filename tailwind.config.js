1c8262a3c83742e025627a5213bf7cfeea4dc1b7
Anderson <loonander@gmail.com>
2025-02-11 17:50:21 -0300

    fix: change in screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,9 @@ export default {
  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
  theme: {
   screens: {
-   sm: '640px',
-   md: '768px',
-   lg: '1024px',
-   xl: '1280px',
-   '2xl': '1536px',
+   sm: '576px',
+   md: '960px',
+   lg: '1440px',
   },
 
   fontFamily: {
