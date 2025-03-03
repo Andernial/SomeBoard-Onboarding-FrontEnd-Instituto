ebe07caba70e3fdd9f7184485024376a5a474286
Anderson <loonander@gmail.com>
2025-03-03 15:38:45 -0300

    feat: apollo-graphql setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import App from './App.tsx';
+
+const client = new ApolloClient({
+ uri: import.meta.env.VITE_API_URL,
+ cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById('root')!).render(
  <StrictMode>
-  <App />
+  <ApolloProvider client={client}>
+   <App />
+  </ApolloProvider>
  </StrictMode>,
 );
