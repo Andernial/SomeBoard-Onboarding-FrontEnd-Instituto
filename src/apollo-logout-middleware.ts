a8a35520e844b6eeb723b678277d2221cd098356
Anderson <loonander@gmail.com>
2025-03-15 20:14:09 -0300

    fix: clean user storage

--- src/apollo-logout-middleware.ts
+++ src/apollo-logout-middleware.ts
@@ -1,6 +1,7 @@
 import { onError } from '@apollo/client/link/error';
 import { GraphQLFormattedError } from 'graphql';
 import { useAuthStorage } from './app/stores/auth.store';
+import { useUserStorage } from './app/stores/user.store';
 
 interface ApiGraphqlError extends GraphQLFormattedError {
  code?: number;
@@ -9,11 +10,13 @@ interface ApiGraphqlError extends GraphQLFormattedError {
 
 export const errorLink = onError(({ graphQLErrors }) => {
  const { token, removeToken } = useAuthStorage.getState();
+ const { removeUser } = useUserStorage.getInitialState();
 
  if (graphQLErrors) {
   graphQLErrors.forEach((error: ApiGraphqlError) => {
    if (error.code === 401 && token) {
     removeToken();
+    removeUser();
    }
   });
  }
