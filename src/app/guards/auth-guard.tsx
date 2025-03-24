203435ae55857c79fec770ad13ab21771f06758a
Anderson <loonander@gmail.com>
2025-03-24 17:41:30 -0300

    fix: remove unnecessary use effect

--- src/app/guards/auth-guard.tsx
+++ src/app/guards/auth-guard.tsx
@@ -1,19 +1,11 @@
-import { useEffect } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom'; 
+import { Navigate, Outlet } from 'react-router-dom';
 import { useAuthStorage } from '@/app/stores/auth.store';
 
 export function AuthGuard() {
  const token = useAuthStorage((state) => state.token);
- const navigate = useNavigate();
-
- useEffect(() => {
-  if (!token) {
-   navigate('/login');
-  }
- }, [token, navigate]);
 
  if (!token) {
-  return null;
+  return <Navigate to="/login" replace />;
  }
 
  return <Outlet />;
