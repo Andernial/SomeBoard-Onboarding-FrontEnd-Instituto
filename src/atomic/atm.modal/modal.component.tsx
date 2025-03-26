ed6df83aa053b4d454e17c1f5b9bfa781120ad65
Anderson <loonander@gmail.com>
2025-03-26 16:16:16 -0300

    feat: modal-component

---
+++ src/atomic/atm.modal/modal.component.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { createPortal } from 'react-dom';
+import close from '@assets/icons/close.png';
+
+interface modalProps {
+ isOpen: boolean;
+ onClose: () => void;
+ children: React.ReactNode;
+}
+
+export function Modal({ isOpen, onClose, children }: modalProps) {
+ if (!isOpen) {
+  return null;
+ }
+
+ return createPortal(
+  <>
+   <div
+    className="fixed flex justify-center items-center inset-[0px] min-h-svh min-w-full w-full h-full bg-grayScale-xdark bg-opacity-50  z-10"
+    onClick={() => onClose()}
+   />
+
+   <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-grayScale-white rounded-md flex flex-col items-center min-h-[200px] min-w-[400px] z-20">
+    <button className="bg-transparent cursor-pointer self-end mr-sm mt-sm" onClick={() => onClose()}>
+     <img src={close} alt="fechar" />
+    </button>
+    {children}
+   </div>
+  </>,
+  document.body,
+ );
+}
